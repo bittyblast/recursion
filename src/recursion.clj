df64d88821ced05483b4b27b0f2e690ec0ec11ec
bittyblast <heidi.nyman@helsinki.fi>
2017-01-05 13:43:07 +0200

    completed exercises 1 to 7

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,22 +1,36 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (* (first coll)
+       (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (if (or ((complement empty?) (rest coll)) (empty? coll)) false true))
 
 (defn my-last [coll]
-  :-)
+  (cond
+    (empty? coll)              nil
+    ((complement coll?) coll)  coll
+    (singleton? coll)          (first coll)
+    :else                      (my-last (rest coll))
+  ))
 
 (defn max-element [a-seq]
-  :-)
+  (cond
+    (empty? a-seq)            nil
+    (singleton? a-seq)        (first a-seq)
+    :else                     (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2)) seq-1 seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+   (cond
+    (empty? a-seq)             nil
+    (singleton? a-seq)         (first a-seq)
+    :else                      (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
   [:-])
